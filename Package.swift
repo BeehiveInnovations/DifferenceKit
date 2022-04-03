98b1d0191968cfc773e052a8d4cd446e0b710714
Fahad <fahad@guidedways.com>
2022-04-03 17:43:17 +0400

    Dynamic lib

--- Package.swift
+++ Package.swift
@@ -3,23 +3,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "DifferenceKit",
-    platforms: [
-        .iOS(.v9), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)
-    ],
-    products: [
-        .library(name: "DifferenceKit", targets: ["DifferenceKit"])
-    ],
-    targets: [
-        .target(
-            name: "DifferenceKit",
-            path: "Sources"
-        ),
-        .testTarget(
-            name: "DifferenceKitTests",
-            dependencies: ["DifferenceKit"],
-            path: "Tests"
-        )
-    ],
-    swiftLanguageVersions: [.v4_2, .v5]
+  name: "DifferenceKit",
+  platforms: [
+    .iOS(.v9), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)
+  ],
+  products: [
+    .library(name: "DifferenceKitDynamic", type: .dynamic, targets: ["DifferenceKit"]),
+    .library(name: "DifferenceKit", targets: ["DifferenceKit"])
+  ],
+  targets: [
+    .target(
+      name: "DifferenceKit",
+      path: "Sources"
+    ),
+    .testTarget(
+      name: "DifferenceKitTests",
+      dependencies: ["DifferenceKit"],
+      path: "Tests"
+    )
+  ],
+  swiftLanguageVersions: [.v4_2, .v5]
 )
