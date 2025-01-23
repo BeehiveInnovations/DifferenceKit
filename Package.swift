a0cc704a282e276f489190288a5567b9d2eaf737
Fahad <fahad@2doapp.com>
2025-01-23 20:20:03 +0400

    Cleanup

--- Package.swift
+++ Package.swift
@@ -5,19 +5,15 @@ import PackageDescription
 let package = Package(
   name: "DifferenceKit",
   platforms: [
-    .iOS(.v13), .macOS(.v10_15), .tvOS(.v12), .watchOS(.v9)
+    .iOS(.v13), .macOS(.v10_15), .tvOS(.v9), .watchOS(.v2)
   ],
   products: [
-    .library(name: "DifferenceKitDynamic", type: .dynamic, targets: ["DifferenceKit"]),
-    .library(name: "DifferenceKit", targets: ["DifferenceKit"])
+    .library(name: "DifferenceKitDynamic", type: .dynamic, targets: ["DifferenceKit"])
   ],
   targets: [
     .target(
       name: "DifferenceKit",
-      path: "Sources",
-      linkerSettings: [
-        .unsafeFlags(["-Wl,-make_mergeable"])
-      ]
+      path: "Sources"
     ),
     .testTarget(
       name: "DifferenceKitTests",
